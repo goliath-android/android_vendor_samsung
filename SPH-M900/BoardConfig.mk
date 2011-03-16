a28ff99ac763d2f86247e8b5064d3afdb6661bb0
Jared Szechy <android@goliathonline.com>
2011-03-16 12:25:53 -0400

    first commit

---
+++ SPH-M900/BoardConfig.mk
@@ -0,0 +1,7 @@
+# These definitions override the defaults in config/config.make for SPH-M900
+#
+# TARGET_NO_BOOTLOADER := false
+#
+TARGET_USE_GENERIC_AUDIO := true
+
+TARGET_CPU_ABI := armeabi
---
+++ products/SPH-M900.mk
@@ -0,0 +1,8 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic.mk)
+#
+# Overrides
+PRODUCT_NAME := moment
+PRODUCT_DEVICE := SPH-M900
+PRODUCT_MODEL := SPH-M900
+PRODUCT_BRAND := Sprint
+PRODUCT_MANUFACTURER := Samsung
